49eade620d43f6d35b8905a4a7b2e0d0b066b3b7
Allwize <github@allwize.io>
2018-05-08 12:00:53 +0200

    Fix tests that return a value

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -137,37 +137,35 @@ testF(CustomTest, get_control_field) {
 }
 
 testF(CustomTest, set_default_key) {
+    uint8_t value = 0x10;
     uint8_t key[16];
-    for (uint8_t i=0; i<16; i++) key[i] = 0x10;
+    for (uint8_t i=0; i<sizeof(key); i++) key[i] = value;
     allwize->setDefaultKey(key);
     uint8_t expected[] = {
         0x00, 'M',
-        0x40, 0x10, 0x41, 0x10, 0x42, 0x10, 0x43, 0x10,
-        0x44, 0x10, 0x45, 0x10, 0x46, 0x10, 0x47, 0x10,
-        0x48, 0x10, 0x49, 0x10, 0x4A, 0x10, 0x4B, 0x10,
-        0x4C, 0x10, 0x4D, 0x10, 0x4E, 0x10, 0x4F, 0x10,
+        0x40, value, 0x41, value, 0x42, value, 0x43, value,
+        0x44, value, 0x45, value, 0x46, value, 0x47, value,
+        0x48, value, 0x49, value, 0x4A, value, 0x4B, value,
+        0x4C, value, 0x4D, value, 0x4E, value, 0x4F, value,
         0xFF, 0x58
     };
     compare(sizeof(expected), expected);
 }
 
 testF(CustomTest, get_temperature) {
-    uint8_t value = allwize->getTemperature();
-    if (32 != value) return false;
+    assertEqual(32, (int) allwize->getTemperature());
     uint8_t expected[] = {0x00, 'U', 0x58};
     compare(sizeof(expected), expected);
 }
 
 testF(CustomTest, get_voltage) {
-    uint16_t value = allwize->getVoltage();
-    if (4800 != value) return false;
+    assertEqual(4800, (int) allwize->getVoltage());
     uint8_t expected[] = {0x00, 'V', 0x58};
     compare(sizeof(expected), expected);
 }
 
 testF(CustomTest, get_rssi) {
-    float value = allwize->getRSSI();
-    if (-80 != value) return false;
+    assertEqual(-80, (int) allwize->getRSSI());
     uint8_t expected[] = {0x00, 'S', 0x58};
     compare(sizeof(expected), expected);
 }
@@ -180,10 +178,6 @@ void setup() {
 
     Serial.begin(115200);
     while (!Serial);
-
-    Serial.println();
-    Serial.println("Allwize library test suite");
-    Serial.println();
 
     //TestRunner::setVerbosity(Verbosity::kAll);
 
