04d2deda3a341999f73182e0af3085aa9fe7d664
Allwize <github@allwize.io>
2018-10-15 17:19:48 +0200

    MH-Z16 CO2 example

---
+++ examples/sensor-mhz16/sensor-mhz16.ino
@@ -0,0 +1,148 @@
+/*
+
+AllWize - MH-Z16 CO2 Sensor Example
+
+Sends the data for MH-Z16 sensor.
+It uses the PWM output connected to the AllWize K1 digital grove connector.
+
+Copyright (C) 2018 by AllWize <github@allwize.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// -----------------------------------------------------------------------------
+// Board definitions
+// -----------------------------------------------------------------------------
+
+#if defined(ARDUINO_AVR_UNO)
+    #define RESET_PIN           7
+    #define RX_PIN              8
+    #define TX_PIN              9
+    #define DEBUG_SERIAL        Serial
+#endif // ARDUINO_AVR_UNO
+
+#if defined(ARDUINO_AVR_LEONARDO)
+    #define RESET_PIN           7
+    #define HARDWARE_SERIAL     Serial1
+    #define DEBUG_SERIAL        Serial
+#endif // ARDUINO_AVR_LEONARDO
+
+#if defined(ARDUINO_ARCH_SAMD)
+    #define RESET_PIN           7
+    #define HARDWARE_SERIAL     Serial1
+    #define DEBUG_SERIAL        SerialUSB
+#endif // ARDUINO_ARCH_SAMD
+
+// -----------------------------------------------------------------------------
+// Configuration
+// -----------------------------------------------------------------------------
+
+#define WIZE_CHANNEL        CHANNEL_11
+#define WIZE_POWER          POWER_20dBm
+#define WIZE_DATARATE       DATARATE_2400bps
+#define WIZE_NODE_ID        0x0B
+
+#define MHZ16_PWM           5
+
+#define SEND_INTERVAL       5000
+
+// -----------------------------------------------------------------------------
+// AllWize
+// -----------------------------------------------------------------------------
+
+#include "AllWize.h"
+AllWize * allwize;
+
+void wizeSetup() {
+
+    // Create and init AllWize object
+    #if defined(HARDWARE_SERIAL)
+        allwize = new AllWize(&HARDWARE_SERIAL, RESET_PIN);
+    #else
+        allwize = new AllWize(RX_PIN, TX_PIN, RESET_PIN);
+    #endif
+    allwize->begin();
+    if (!allwize->waitForReady()) {
+        DEBUG_SERIAL.println("Error connecting to the module, check your wiring!");
+        while (true);
+    }
+
+    allwize->slave();
+    allwize->setChannel(WIZE_CHANNEL, true);
+    allwize->setPower(WIZE_POWER);
+    allwize->setDataRate(WIZE_DATARATE);
+    allwize->setControlInformation(WIZE_NODE_ID);
+
+}
+
+void wizeSend(const char * payload) {
+
+    DEBUG_SERIAL.print("[AllWize] Payload: ");
+    DEBUG_SERIAL.println(payload);
+
+    if (!allwize->send(payload)) {
+        DEBUG_SERIAL.println("[AllWize] Error sending message");
+    }
+
+}
+
+// -----------------------------------------------------------------------------
+// Sensor setup
+// -----------------------------------------------------------------------------
+
+void sensorSetup() {
+    pinMode(MHZ16_PWM, INPUT);
+}
+
+unsigned long sensorPPM() {
+    unsigned long ms = pulseIn(MHZ16_PWM, HIGH, 2000000) / 1000;
+    return (ms > 2) ? 2 * (ms - 2) : 0;
+}
+
+// -----------------------------------------------------------------------------
+// Main
+// -----------------------------------------------------------------------------
+
+void setup() {
+
+    // Init serial DEBUG_SERIAL
+    DEBUG_SERIAL.begin(115200);
+    while (!DEBUG_SERIAL && millis() < 5000);
+    DEBUG_SERIAL.println();
+    DEBUG_SERIAL.println("[AllWize] MH-Z16 CO2 sensor example");
+    DEBUG_SERIAL.println();
+
+    // Init sensor
+    sensorSetup();
+
+    // Init radio
+    wizeSetup();
+
+}
+
+void loop() {
+
+    unsigned long co2 = sensorPPM();
+    if (0 != co2) {
+        char payload[7];
+        itoa(co2, payload, 10);
+        wizeSend(payload);
+    } else {
+        DEBUG_SERIAL.println("[SENSOR] Error reading sensor");
+    }
+
+    delay(SEND_INTERVAL);
+
+}
