d8bfc17d0e1ae5852c60d877cfa96202519517f5
Allwize <github@allwize.io>
2018-12-03 13:16:52 +0100

    Fix lowpower entry and exit points

--- examples/lowpower/lowpower.ino
+++ examples/lowpower/lowpower.ino
@@ -4,7 +4,7 @@ Allwize - Low Power Example
 
 Pretty-prints out the configuration settings stored in the module non-volatile memory.
 
-Copyright (C) 2018 by Allwize <github@allwize.io>
+Copyright (C) 2018 by AllWize <github@allwize.io>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,11 +22,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // -----------------------------------------------------------------------------
-// Config & globals
+// Configuration
 // -----------------------------------------------------------------------------
 
-#include "Allwize.h"
-Allwize * allwize;
+#define WIZE_CHANNEL        CHANNEL_11
+#define WIZE_POWER          POWER_14dBm
+#define WIZE_DATARATE       DATARATE_2400bps
+#define WIZE_NODE_ID        0x09
 
 // -----------------------------------------------------------------------------
 // Board definitions
@@ -51,7 +53,7 @@ Allwize * allwize;
     #define DEBUG_SERIAL        Serial
 
     // Using hardware serial:
-    #define MODULE_SERIAL       Serial1
+    #define HARDWARE_SERIAL     Serial1
 
 
 #endif // ARDUINO_AVR_LEONARDO
@@ -63,63 +65,48 @@ Allwize * allwize;
     #define DEBUG_SERIAL        SerialUSB
 
     // Using exposed hardware serials:
-    #define MODULE_SERIAL       Serial1
+    #define HARDWARE_SERIAL     Serial1
 
 
 #endif // ARDUINO_ARCH_SAMD
 
-#if defined(ARDUINO_ARCH_ESP8266)
-
-    // Common:
-    #define RESET_PIN           14
-    #define DEBUG_SERIAL        Serial
-
-    // Using Software serial:
-    #define RX_PIN              12
-    #define TX_PIN              13
-
-#endif // ARDUINO_ARCH_ESP8266
-
-#if defined(ARDUINO_ARCH_ESP32)
-
-    // Common:
-    #define RESET_PIN           14
-    #define DEBUG_SERIAL        Serial
-
-    // Using Hardware serial on random GPIOs:
-    #define RX_PIN              12
-    #define TX_PIN              13
-
-#endif // ARDUINO_ARCH_ESP32
-
 // -----------------------------------------------------------------------------
-// Radio
+// AllWize
 // -----------------------------------------------------------------------------
+
+#include "AllWize.h"
+AllWize * allwize;
 
 void wizeSetup() {
 
-    DEBUG_SERIAL.println("Checking radio module");
-
-    #if defined(ARDUINO_ARCH_SAMD) && defined(RX_PIN) && defined(TX_PIN)
-        pinPeripheral(RX_PIN, SERCOM_MODE);
-        pinPeripheral(TX_PIN, SERCOM_MODE);
-    #endif
-
     // Create and init AllWize object
-    #if defined(MODULE_SERIAL)
-        allwize = new Allwize(&MODULE_SERIAL, RESET_PIN);
+    #if defined(HARDWARE_SERIAL)
+        allwize = new AllWize(&HARDWARE_SERIAL, RESET_PIN);
     #else
-        allwize = new Allwize(RX_PIN, TX_PIN, RESET_PIN);
+        allwize = new AllWize(RX_PIN, TX_PIN, RESET_PIN);
     #endif
-
     allwize->begin();
-
     if (!allwize->waitForReady()) {
         DEBUG_SERIAL.println("Error connecting to the module, check your wiring!");
         while (true);
     }
 
-    DEBUG_SERIAL.println("Radio module OK");
+    allwize->slave();
+    allwize->setChannel(WIZE_CHANNEL, true);
+    allwize->setPower(WIZE_POWER);
+    allwize->setDataRate(WIZE_DATARATE);
+    allwize->setControlInformation(WIZE_NODE_ID);
+
+}
+
+void wizeSend(const char * payload) {
+
+    DEBUG_SERIAL.print("[AllWize] Payload: ");
+    DEBUG_SERIAL.println(payload);
+
+    if (!allwize->send(payload)) {
+        DEBUG_SERIAL.println("[AllWize] Error sending message");
+    }
 
 }
 
@@ -147,29 +134,35 @@ void rtcSetup() {
 
 void sleep() {
 
+    // Sleep the radio
     allwize->sleep();
+    delay(10);
 
+    // Sleep the MCU
     #if defined(ARDUINO_ARCH_SAMD)
         rtc.setEpoch(datetime);
-        rtc.setAlarmEpoch(datetime + 2);
+        rtc.setAlarmEpoch(datetime + 8);
         rtc.enableAlarm(rtc.MATCH_HHMMSS);
-        //USBDevice.detach();
+        USBDevice.detach();
         LowPower.standby();
     #else
-        LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
+    	LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
     #endif
 
 }
 
 void wakeup() {
 
+    // Wake up the radio
     allwize->wakeup();
 
+    // Wake up the MCU peripherials
     #if defined(ARDUINO_ARCH_SAMD)
-        //USBDevice.attach();
+        USBDevice.attach();
     #endif
-
-    while(!SerialUSB);
+    
+    // Wait for debug port to be enabled
+    while(!DEBUG_SERIAL);
 
 }
 
@@ -179,19 +172,25 @@ void wakeup() {
 
 void setup() {
 
+    // Init DEBUG_SERIAL
     DEBUG_SERIAL.begin(115200);
-    while (!DEBUG_SERIAL && millis() < 5000);
+    while (!DEBUG_SERIAL && millis() < 2000);
     DEBUG_SERIAL.println();
     DEBUG_SERIAL.println("Allwize - Low Power");
     DEBUG_SERIAL.println();
 
     // -------------------------------------------------------------------------
 
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
     wizeSetup();
 
     #if defined(ARDUINO_ARCH_SAMD)
-    rtcSetup();
+        rtcSetup();
     #endif
+
+    DEBUG_SERIAL.println("Ready");
 
 }
 
@@ -199,12 +198,16 @@ void loop() {
 
     // -------------------------------------------------------------------------
 
-    DEBUG_SERIAL.println("Idle for 100s");
-    delay(100);
+    digitalWrite(LED_BUILTIN, HIGH);
+    wizeSend("U");
+    DEBUG_SERIAL.println("Idle for 8s");
+    delay(8000);
+    digitalWrite(LED_BUILTIN, LOW);
 
     // -------------------------------------------------------------------------
 
-    DEBUG_SERIAL.println("Going into low-power mode for 2s");
+    DEBUG_SERIAL.println("Going into low-power mode for 8s");
+    delay(10);
     sleep();
     wakeup();
 
