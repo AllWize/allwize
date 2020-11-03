d519ef32bd615a6817120edd66657d3aa89eb588
Allwize <github@allwize.io>
2020-11-03 11:41:06 +0100

    First ping-pong version

---
+++ examples/ping-pong/ping-pong_slave_allwize_k2/ping-pong_allwize_k2.ino
@@ -0,0 +1,151 @@
+/*
+
+AllWize - Ping-pong examle - AllWize K2
+
+Listens to messages on the same channel and data rate,
+prints them out via the serial monitor and
+send bounces them out again.
+
+Copyright (C) 2020 by AllWize <github@allwize.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "AllWize.h"
+
+// -----------------------------------------------------------------------------
+// Board configuration
+// -----------------------------------------------------------------------------
+
+// Check http://wiki.allwize.io/index.php?title=Allwize_K2#Arduino_IDE
+// to add support to Allwize K2 board in Arduino IDE
+#if not defined(ARDUINO_ALLWIZE_K2)
+    #error "This example is meant to run on an AllWize K2 board!"
+#endif
+
+#define MODULE_SERIAL           SerialWize
+#define DEBUG_SERIAL            SerialUSB
+#define RESET_PIN               PIN_WIZE_RESET
+
+// -----------------------------------------------------------------------------
+// Configuration
+// -----------------------------------------------------------------------------
+
+#define WIZE_CHANNEL            CHANNEL_04
+#define WIZE_POWER              POWER_14dBm
+#define WIZE_DATARATE           DATARATE_4800bps
+#define WIZE_UID                0x20212223
+
+// -----------------------------------------------------------------------------
+// Globals
+// -----------------------------------------------------------------------------
+
+AllWize allwize(&MODULE_SERIAL, RESET_PIN);
+
+// -----------------------------------------------------------------------------
+// AllWize
+// -----------------------------------------------------------------------------
+
+void wizeSetup() {
+
+    DEBUG_SERIAL.println("Initializing radio module");
+
+    // Init AllWize object
+    allwize.begin();
+    if (!allwize.waitForReady()) {
+        DEBUG_SERIAL.println("[WIZE] Error connecting to the module, check your wiring!");
+        while (true);
+    }
+
+    allwize.master();
+    allwize.setChannel(WIZE_CHANNEL, true);
+    allwize.setPower(WIZE_POWER);
+    allwize.setDataRate(WIZE_DATARATE);
+    allwize.setUID(WIZE_UID);
+
+    char buffer[64];
+    snprintf(buffer, sizeof(buffer), "[WIZE] Listening... CH %d, DR %d\n", allwize.getChannel(), allwize.getDataRate());
+    DEBUG_SERIAL.print(buffer);
+
+}
+
+void wizeDebug() {
+
+        // Get the RAW message
+        uint8_t * message = allwize.getBuffer();
+        uint8_t length = allwize.getLength();
+
+        // Print it
+        char buffer[6];
+        DEBUG_SERIAL.print("[WIZE] Received: ");
+        for (uint8_t i=1; i<length-1; i++) { // first & last bytes are start & stop bytes, no need to print them
+            snprintf(buffer, sizeof(buffer), "%02X", message[i]);
+            DEBUG_SERIAL.print(buffer);
+        }
+        DEBUG_SERIAL.println();
+
+}
+
+void wizeSend(const char * payload) {
+
+    char buffer[64];
+    snprintf(buffer, sizeof(buffer), "[WIZE] Sending '%s'\n", payload);
+    DEBUG_SERIAL.print(buffer);
+
+    if (!allwize.send(payload)) {
+        DEBUG_SERIAL.println("[WIZE] Error sending message");
+    }
+
+}
+
+void wizeLoop() {
+
+    if (allwize.available()) {
+
+        // Print message to console
+        wizeDebug();
+
+        // Send back
+        allwize_message_t message = allwize.read();
+        allwize.send(message.data, message.len);
+
+    }
+
+}
+
+// -----------------------------------------------------------------------------
+// Main
+// -----------------------------------------------------------------------------
+
+void setup() {
+
+    // Setup serial DEBUG_SERIAL
+    DEBUG_SERIAL.begin(115200);
+    while (!DEBUG_SERIAL && millis() < 5000);
+    DEBUG_SERIAL.println();
+    DEBUG_SERIAL.println("[MAIN] Wize Ping-Pong Example");
+    DEBUG_SERIAL.println();
+
+    // Init radio
+    wizeSetup();
+
+}
+
+void loop() {
+
+    // Listen to messages
+    wizeLoop();
+
+}
