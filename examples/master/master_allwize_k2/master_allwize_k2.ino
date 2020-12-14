528586ed75bd356cd671fe0738e821f565c476f5
Allwize <github@allwize.io>
2020-12-14 10:51:05 +0100

    Remove reference to deprecated operator_id

--- examples/master/master_allwize_k2/master_allwize_k2.ino
+++ examples/master/master_allwize_k2/master_allwize_k2.ino
@@ -86,7 +86,7 @@ void wizeDebugMessage(allwize_message_t message) {
             message.man,
             message.address[0], message.address[1],
             message.address[2], message.address[3],
-            message.wize_control, message.wize_operator_id, message.wize_application, message.wize_counter,
+            message.wize_control, message.wize_network_id, message.wize_application, message.wize_counter,
             (int16_t) message.rssi / -2
         );
     } else {
